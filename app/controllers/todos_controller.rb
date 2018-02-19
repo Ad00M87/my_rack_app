985a5f509f638beac843dcc3ff3a5592dd4efc02
Adam Kimball <adamchasekimball@gmail.com>
2018-02-19 10:34:02 -0700

    adding models

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,35 +5,27 @@ class TodosController < BaseController
   # GET /todos
   def index
     @title = "Todos"
-    @todos = todos
-
+    @todos = Todo.all
     server_response render_template
   end
 
   # GET /todos/:id
   def show
-    @todo = todos.find { |t| t[:id] == params[:id].to_i }
-    @title = "#{@todo[:name]} page"
+    @todo = Todo.find(params[:id])
+    @title = "#{@todo.name}'s page"
     server_response render_template
   end
 
   # GET /todos/new
   def new
-    @title = "Add an item"
+    @title = "Add a todo item"
     server_response render_template
   end
 
   # POST /todos
   def create
-    redirect_to "todos/"
+    todo = Todo.new(name: params['todo']['name'])
+    todo.save
+    redirect_to "todos/#{todo.id}"
   end
-
-  private
-    def todos
-      [
-        { id: 1, name: 'Learn Ruby' },
-        { id: 2, name: 'Build a server' },
-        { id: 3, name: 'Profit' }
-      ]
-    end
 end
